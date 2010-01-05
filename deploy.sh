102cfa05a3a805a93ea0db845a077892643493a4
Stuart Powers <stuart.powers@gmail.com>
2010-01-05 11:58:58 -0500

    modifications to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,56 @@
 
 git-ls-files | while read file; do
    if [ ! -f $HOME/$file ]; then
-      echo "$HOME/$file -- does not exist"
+#      echo "------- $HOME/$file -- does not exist"
+   ls -lrt >/dev/null;
    else
-      cmp $HOME/$file $file
+   #   cmp $HOME/$file $file
+   ls -lrt >/dev/null;
    fi
 done
+
+
+function mydiff()
+{
+   if [ -x /usr/bin/colordiff ]; then
+      COLORDIFF="colordiff"
+   else
+      COLORDIFF="while read line; do echo $line; done"
+   fi 
+      
+   echo "/usr/bin/diff --suppress-common-lines --side-by-side $@ $COLORDIFF"
+   /usr/bin/diff --suppress-common-lines --side-by-side $@ | $COLORDIFF
+}
+
+#function prettydiff ()
+#{
+#   /usr/bin/diff --suppress-common-lines --side-by-side $@ | colordiff
+#}
+#
+#test -x /usr/bin/diff      && mydiff=normaldiff
+#test -x /usr/bin/colordiff && mydiff=prettydiff
+#
+#
+#
+git-ls-files | while read file; do
+   if [ -f $HOME/$file ]; then
+      cmp $HOME/$file $file >/dev/null
+      if [ $? -ne 0 ]; then
+         ls -l "$HOME/$file" "$file"
+         #echo "colordiff --suppress-common-lines --side-by-side $HOME/$file $file"
+         #/usr/bin/colordiff --suppress-common-lines --side-by-side $HOME/$file $file
+
+         
+         mydiff "$HOME/$file" "$file"
+         #/usr/bin/diff --suppress-common-lines --side-by-side "$HOME/$file" "$file"  | colordiff
+         echo "---------------------------------------------------------------------------------"
+
+      fi
+   fi
+done
+
+
+#diff --suppress-common-lines --side-by-side
 
 
 
